3aa761ad7fefcc9052340207e3b9a9758ca6152b
netovs@gmail.com <netovs@gmail.com>
2021-10-05 14:55:33 -0500

    Actualización datos de usuario más relevantes 30 09 2021

--- application/controllers/Services.php
+++ application/controllers/Services.php
@@ -23,7 +23,7 @@ class Services extends CI_Controller
     public function actualiza_usuario() {
         $data = $_REQUEST;
         unset($data['ci_session']);
-        print_r($data);
+        // print_r($data);
         // updateUser 
         $result     = $this->start_session_one->updateUser($data['id'], $data);
         $updateUser = 1; // $result->result_id->num_rows;
